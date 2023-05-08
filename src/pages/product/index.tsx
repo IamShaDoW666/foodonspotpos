5b870896a82481d48ef3e66233676026b0189d4d
IamShaDoW666 <www.gaming2018@gmail.com>
2023-05-08 22:55:33 +0530

    created PosLayout + working on product CRUD page

---
+++ src/pages/product/index.tsx
@@ -0,0 +1,15 @@
+import PosLayout from "@/layouts/PosLayout";
+
+const Product = () => {
+    return (
+        <PosLayout>
+            <div className="h-full w-full rounded-3xl p-4">
+                <div className="bg-white shadow-xl rounded-3xl w-full h-full">
+                    {/* Product CRUD Page here */}
+                </div>
+            </div>
+        </PosLayout>
+    );
+}
+
+export default Product;
